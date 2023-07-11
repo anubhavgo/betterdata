11ff983d1c01b4d415b37ad1e70664192d64cd04
Anubhav <anubhav@Anubhavs-MacBook-Pro-2.local>
2023-07-11 23:32:30 +0530

    Added circleci and ebs file

--- database.py
+++ database.py
@@ -5,7 +5,13 @@ import os
 
 SQLALCHEMY_DATABASE_URL = os.environ["POSTGRES_URL"]
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL,pool_pre_ping=True,
+                        connect_args={
+                            "keepalives": 1,
+                            "keepalives_idle": 30,
+                            "keepalives_interval": 10,
+                            "keepalives_count": 5,
+                        })
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
